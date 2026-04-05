682a13a09c4c8d0147a812043f53d4ad1c5ef7e1
Mamdouh Ahmed Ezzeldin <m.ezz@nu.edu.eg>
2026-04-05 11:58:05 +0200

    functions problem

---
+++ C++/Functions/Functions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int largest = 0;
+    largest = a>b? a:b;
+    largest = largest>c? largest:c;
+    largest = largest>d? largest:d;
+    return largest;
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
